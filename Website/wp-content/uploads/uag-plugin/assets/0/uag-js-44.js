e9ab3790bae1d7bdcc96d951bff14e9fa349c821
U Manish <129829238+ZentoManish@users.noreply.github.com>
2024-10-29 14:10:30 +0530

    Add files via upload

---
+++ Website/wp-content/uploads/uag-plugin/assets/0/uag-js-44.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dfff009f' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a4b0df7d' );
+});
+ });
